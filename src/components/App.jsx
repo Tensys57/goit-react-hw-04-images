809946ab12f6e10f143a7b3f12ecb7b2aab085c7
Tensys57 <tensys57@gmail.com>
2023-06-07 16:21:13 +0300

    next step

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,104 +1,78 @@
-import { Component } from 'react';
-
 import { Searchbar } from './Searchbar/Searchbar';
 import { getPhotos } from '../Service/PhotoService';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 import { Button } from './Button/Button';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-export class App extends Component {
-  state = {
-    hits: [],
-    query: '',
-    isLoading: false,
-    isEmpty: false,
-    showBtn: false,
-    error: '',
-    page: 1,
-    largeImageURL: '',
-  };
+export const App = () => {
+  const [hits, setHits] = useState([]);
+  const [query, setQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [showBtn, setShowBtn] = useState(false);
+  const [error, setError] = useState('');
+  const [page, setPage] = useState(1);
+  const [largeImageURL, setLargeImageURL] = useState('');
 
-  onSubmit = query => {
-    this.setState({ query });
-    if (this.state.query === query) {
+  useEffect(() => {
+    if (!query) return;
+    setIsLoading(true);
+    getPhotos(query, page)
+      .then(({ hits, totalHits }) => {
+        if (!hits.length) {
+          setIsEmpty(true);
+          return;
+        }
+        setHits(prevState => [...prevState, ...hits]);
+        setShowBtn(page < Math.ceil(totalHits / 12));
+      })
+      .catch(error => {
+        setError(error.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [query, page]);
+
+  const onSubmit = Newquery => {
+    if (query === Newquery) {
       return alert('Already shown');
     }
-    this.setState({
-      query,
-      page: 1,
-      photos: [],
-      showBtn: false,
-      isEmpty: false,
-      error: '',
-    });
+
+    setQuery(query);
+    setPage(1);
+    setHits([]);
+    setShowBtn(false);
+    setIsEmpty(false);
+    setError('');
   };
 
-  componentDidUpdate(_, prevState) {
-    const { query, page } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
-      this.setState({ isLoading: true });
-      getPhotos(query, page)
-        .then(({ hits, totalHits }) => {
-          if (!hits.length) {
-            this.setState({ isEmpty: true });
-            return;
-          }
-          this.setState(prevState => ({
-            hits: [...prevState.hits, ...hits],
-            showBtn: page < Math.ceil(totalHits / 12),
-          }));
-        })
-        .catch(error => {
-          this.setState({ error: error.message });
-        })
-        .finally(() => {
-          this.setState({ isLoading: false });
-        });
-    }
-  }
-
-  handleClick = () => {
-    this.setState(prev => ({
-      page: prev.page + 1,
-    }));
+  const handleClick = () => {
+    setPage(prevState => prevState + 1);
   };
 
-  showModal = largeImageURL => {
-    this.setState({ largeImageURL });
+  const showModal = largeImageURL => {
+    setLargeImageURL(largeImageURL);
   };
 
-  render() {
-    const { hits, isLoading, isEmpty, showBtn, largeImageURL, error } =
-      this.state;
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Searchbar onSubmit={this.onSubmit} />
-        {hits.length > 0 && (
-          <ImageGallery photos={hits} openModal={this.showModal} />
-        )}
-        {showBtn && <Button handleClick={this.handleClick} />}
-        {isLoading && <Loader />}
-        {largeImageURL && (
-          <Modal
-            largeImageURL={largeImageURL}
-            tags={hits.tags}
-            closeModal={this.showModal}
-          />
-        )}
-        {isEmpty && <p>Sorry. There are no images ... 😭</p>}
-        {error && <p>Sorry. {error} ... 😭</p>}
-      </div>
-    );
-  }
-}
+  return (
+    <>
+      <Searchbar onSubmit={onSubmit} />
+      {hits.length > 0 && <ImageGallery photos={hits} openModal={showModal} />}
+      {showBtn && <Button handleClick={handleClick} />}
+      {isLoading && <Loader />}
+      {largeImageURL && (
+        <Modal
+          largeImageURL={largeImageURL}
+          tags={hits.tags}
+          closeModal={showModal}
+        />
+      )}
+      {isEmpty && <p>Sorry. There are no images ... 😭</p>}
+      {error && <p>Sorry. {error} ... 😭</p>}
+    </>
+  );
+};
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,41 +1,33 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 
 import css from './Modal.module.css';
 
-export class Modal extends Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyDown);
-  }
+export const Modal = ({ closeModal, largeImageURL, tags }) => {
+  useEffect(() => {
+    const handleKeyDown = ev => {
+      if (ev.key === 'Escape') {
+        closeModal('');
+      }
+    };
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [closeModal]);
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleOverlayClick = ev => {
+  const handleOverlayClick = ev => {
     if (ev.target === ev.currentTarget) {
-      this.props.closeModal('');
+      closeModal('');
     }
   };
 
-  handleKeyDown = ev => {
-    if (ev.key === 'Escape') {
-      this.props.closeModal('');
-    }
-  };
-
-  render() {
-    return (
-      <div className={css.Overlay} onClick={this.handleOverlayClick}>
-        <img
-          src={this.props.largeImageURL}
-          alt={this.props.tags}
-          className={css.Modal}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={css.Overlay} onClick={handleOverlayClick}>
+      <img src={largeImageURL} alt={tags} className={css.Modal} />
+    </div>
+  );
+};
 
 Modal.propTypes = {
   largeImageURL: PropTypes.string.isRequired,
